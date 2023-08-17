73f46ae5f21ff8eae5e9044f0d3a2abf7e38cae5
JEONGSUJONG <jeon95ujong@gmail.com>
2023-08-17 18:53:22 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 0;
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                index = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index + 1);
+    }
+}
