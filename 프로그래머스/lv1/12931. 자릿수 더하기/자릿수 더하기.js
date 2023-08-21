72ab7093fa5c9f6a1a6468f20a41080ec0da7847
JEONGSUJONG <142254876+JEONGSUJONG@users.noreply.github.com>
2023-08-21 14:46:59 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,11 @@
+function solution(n)
+{
+    var answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    while(n>0){
+        answer+=n%10;
+        n=Math.floor(n/10);
+    }
+    return answer;
+}
