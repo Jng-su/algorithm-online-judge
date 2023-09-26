49a971e7e7aa1bcb12d46d4ec021f4fb881e09ca
JEONGSUJONG <142254876+JEONGSUJONG@users.noreply.github.com>
2023-09-26 19:19:35 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        for (int i = 0; i < photo.length; i++) {
+            int totalYearning = 0;
+            
+            for (String person : photo[i]) {
+                for (int j = 0; j < name.length; j++) {
+                    if (person.equals(name[j])) {
+                        totalYearning += yearning[j];
+                        break;
+                    }
+                }
+            }
+            
+            answer[i] = totalYearning;
+        }
+        
+        return answer;
+    }
+}
