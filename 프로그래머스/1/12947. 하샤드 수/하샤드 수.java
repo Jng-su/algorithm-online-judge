9d6f45680aa3004120d3b4e22a89832935499461
JEONGSUJONG <jeon95ujong@gmail.com>
2023-10-14 12:54:29 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int originalX = x;
+
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+
+        if (originalX % sum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
