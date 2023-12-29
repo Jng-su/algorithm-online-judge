2949f9191952b0c41d342078d61d34f29a45dc46
JEONGSUJONG <142254876+JEONGSUJONG@users.noreply.github.com>
2023-12-29 23:35:51 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 252 ms, Memory: 18536 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        int[] baskets = new int[N];
+        for (int i = 0; i < N; i++) {
+            baskets[i] = i + 1;
+        }
+
+        for (int m = 0; m < M; m++) {
+            int start = scanner.nextInt();
+            int end = scanner.nextInt();
+
+            reverseBaskets(baskets, start - 1, end - 1);
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(baskets[i] + " ");
+        }
+    }
+
+    private static void reverseBaskets(int[] baskets, int start, int end) {
+        while (start < end) {
+            // swap
+            int temp = baskets[start];
+            baskets[start] = baskets[end];
+            baskets[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
