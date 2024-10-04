1c3eebf2d03118f7e29caaeeeb5615bd11920c0a
정수종 <jeon95ujong@gmail.com>
2024-10-04 18:20:39 +0900

    [Bronze I] Title: 더하기 사이클, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int originNum = Integer.parseInt(br.readLine());
+        int temp = originNum;
+        int count = 0;
+
+        if (originNum==0) {
+            System.out.println(1);
+            return;
+        }
+       
+        while (true) {
+
+            int tens = temp / 10;
+            int ones = temp % 10;
+            int sumDigits = (tens + ones) % 10;
+
+            temp = (ones * 10) + sumDigits;
+            count++;
+
+            if (originNum == temp) break;
+
+        }
+        System.out.println(count);
+    }
+}
