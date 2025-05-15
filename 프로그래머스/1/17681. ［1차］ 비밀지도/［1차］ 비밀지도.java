efe956c39dc92eca787b6df16226cff576373296
정수종 <jeon95ujong@gmail.com>
2025-05-15 15:39:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 10.67 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,8 +3,12 @@ class Solution {
         String[] result = new String[n];
         for (int i = 0; i < n; i++) {
             String binary = Integer.toBinaryString(arr1[i] | arr2[i]);
-            String padded = String.format("%" + n + "s", binary).replace(' ', '0');
-            result[i] = padded.replace('1', '#').replace('0', ' ');
+            if (binary.length() < n) {
+                binary = String.format("%" + n + "s", binary).replace(' ', '0');
+            } else {
+                binary = binary.substring(binary.length() - n);
+            }
+            result[i] = binary.replace('1', '#').replace('0', ' ');
         }
         return result;
     }
