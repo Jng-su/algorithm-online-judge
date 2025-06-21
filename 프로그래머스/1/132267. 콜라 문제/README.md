07cdb7aeb488cfb15eb8dc11a51964a2d25c4d0d
정수종 <jeon95ujong@gmail.com>
2025-06-21 17:46:31 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/README.md
+++ 프로그래머스/1/132267. 콜라 문제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라 문제 - 132267 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 79.5 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 17:17:15
+2025년 06월 21일 17:46:29
 
 ### 문제 설명
 
--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,13 +1,15 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
+        int result = 0;
         
         while (n >= a) {
-            int exchange = n / a;  // 현재 가지고 있는 빈 병을 교환해서 얻을 수 있는 콜라 수
-            answer += exchange * b;  // 교환한 콜라를 더해줌
-            n = n % a + exchange * b;  // 남은 빈 병 수 계산
+            int trade = n / a;
+            int remain = n % a;
+            
+            result += trade * b;
+            n = trade * b + remain;
         }
         
-        return answer;
+        return result;
     }
 }
